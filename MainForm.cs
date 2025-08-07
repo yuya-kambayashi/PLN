5373a3fcc6f8b584d82517bef3bb58fee8d688ac
yuya-kambayashi <kambayashi73@gmail.com>
2025-08-07 17:55:07 +0900

    プロジェクト ファイルを追加します。

---
+++ MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseCAD
+{
+    internal class MainForm : Form
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MainForm";
+            this.Text = "BaseCAD Application";
+            this.ResumeLayout(false);
+        }
+    }
+}
