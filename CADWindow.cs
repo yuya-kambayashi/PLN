a23dfcedc1212dec67077dfd042ca957c4fcbdf7
yuya-kambayashi <kambayashi73@gmail.com>
2025-08-07 18:09:11 +0900

    CADWindowの追加

---
+++ CADWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseCAD
+{
+    internal class CADWindow : UserControl
+    {
+        public CADWindow()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CADWindow
+            // 
+            this.Name = "CADWindow";
+            this.Size = new System.Drawing.Size(683, 424);
+            this.ResumeLayout(false);
+        }
+    }
+}
--- MainForm.cs
+++ MainForm.cs
@@ -8,6 +8,8 @@ namespace BaseCAD
 {
     internal class MainForm : Form
     {
+        CADWindow cadWindow1;
+        //CADDocument doc;
 
         public MainForm()
         {
@@ -15,14 +17,17 @@ namespace BaseCAD
         }
         private void InitializeComponent()
         {
+            cadWindow1 = new CADWindow();
+
             this.SuspendLayout();
             // 
             // MainForm
             // 
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1008, 516);
             this.Name = "MainForm";
             this.Text = "BaseCAD Application";
             this.ResumeLayout(false);
+            this.BackColor = System.Drawing.Color.Black;
         }
     }
 }
