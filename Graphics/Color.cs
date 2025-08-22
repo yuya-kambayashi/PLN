19d4c0f2feea8c93e287a5e97e045eacea42f165
yuya-kambayashi <kambayashi73@gmail.com>
2025-08-22 10:48:30 +0900

    Commit 18b1429

--- Graphics/Color.cs
+++ Graphics/Color.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 
 namespace BaseCAD.Graphics
@@ -151,6 +152,7 @@ namespace BaseCAD.Graphics
     }
     #endregion
 
+    [TypeConverter(typeof(ColorConverter))]
     public struct Color
     {
         #region Properties
@@ -248,7 +250,33 @@ namespace BaseCAD.Graphics
 
         public string ToHex()
         {
-            return "#" + _argb.ToString("H8");
+            return "#" + _argb.ToString("X8");
+        }
+        public bool IsKnownColor()
+        {
+            if (IsByLayer)
+                return true;
+
+            foreach (uint argb in knownColorLookup.Values)
+            {
+                if (Argb == argb)
+                    return true;
+            }
+            return false;
+        }
+
+        public KnownColor ToKnownColor()
+        {
+            if (IsByLayer)
+                return KnownColor.ByLayer;
+
+            foreach (KeyValuePair<KnownColor, uint> pair in knownColorLookup)
+            {
+                if (Argb == pair.Value)
+                    return pair.Key;
+            }
+
+            return KnownColor.Transparent;
         }
         #endregion
 
