a772fdf9e84bfa091f4b4cdc22ee71e12edbb100
yuya-kambayashi <kambayashi73@gmail.com>
2025-08-23 20:44:50 +0900

    Commit 7bc2656

--- MainForm.cs
+++ MainForm.cs
@@ -198,5 +198,14 @@ namespace BaseCAD
             Renderer renderer = (Renderer)btnRenderer.SelectedItem;
             cadWindow1.View.Renderer = renderer.GetType();
         }
+        private void btnShowGrid_Click(object sender, EventArgs e)
+        {
+            cadWindow1.View.ShowGrid = btnShowGrid.Checked;
+        }
+
+        private void btnShowAxes_Click(object sender, EventArgs e)
+        {
+            cadWindow1.View.ShowAxes = btnShowAxes.Checked;
+        }
     }
 }
