650425cfa6450d83e34eef3d078e0348b9754f2f
yuya-kambayashi <kambayashi73@gmail.com>
2025-08-23 22:25:58 +0900

    Commit af80acb

--- Geometry/Vector2D.cs
+++ Geometry/Vector2D.cs
@@ -118,9 +118,9 @@ namespace BaseCAD.Geometry
         }
         public string ToString(IFormatProvider provider)
         {
-            return ToString("({0:F}, {1:F})", provider);
+            return ToString("{0:F}, {1:F}", provider);
         }
-        public string ToString(string format = "({0:F}, {1:F})", IFormatProvider provider = null)
+        public string ToString(string format = "{0:F}, {1:F}", IFormatProvider provider = null)
         {
             return (provider == null) ?
                 string.Format(format, X, Y) :
