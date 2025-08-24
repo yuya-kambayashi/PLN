42f5ccd6d5b355aaf80f3948239c707834b980dd
yuya-kambayashi <kambayashi73@gmail.com>
2025-08-25 00:20:05 +0900

    Commit a261994

--- Editor/SelectionGetter.cs
+++ Editor/SelectionGetter.cs
@@ -23,9 +23,13 @@ namespace BaseCAD
             // Immediately return existing picked-selection if any
             if (Editor.PickedSelection.Count != 0)
             {
-                SelectionSet picked = Editor.PickedSelection;
+                Editor.CurrentSelection.Clear();
+                foreach (Drawable item in Editor.PickedSelection)
+                {
+                    Editor.CurrentSelection.Add(item);
+                }
                 Editor.PickedSelection.Clear();
-                args.Value = picked;
+                args.Value = Editor.CurrentSelection;
                 args.ContinueAsync = false;
             }
             else
