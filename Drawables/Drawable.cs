4b1c067f1357e78903b51a2d93e2181301a64599
yuya-kambayashi <kambayashi73@gmail.com>
2025-08-25 09:45:55 +0900

    Commit 65f5248

--- Drawables/Drawable.cs
+++ Drawables/Drawable.cs
@@ -1,16 +1,7 @@
-﻿using BaseCAD.Drawables;
-using BaseCAD.Geometry;
+﻿using BaseCAD.Geometry;
 using BaseCAD.Graphics;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms.VisualStyles;
 using Color = BaseCAD.Graphics.Color;
 
 namespace BaseCAD.Drawables
