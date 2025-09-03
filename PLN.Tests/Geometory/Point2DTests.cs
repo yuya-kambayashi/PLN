5075927e84fe5ba185c7ef6614b34938297d7005
yuya-kambayashi <kambayashi73@gmail.com>
2025-09-04 00:08:52 +0900

    Point2D.Transformのテストを追加します

--- PLN.Tests/Geometory/Point2DTests.cs
+++ PLN.Tests/Geometory/Point2DTests.cs
@@ -6,11 +6,42 @@ namespace PLN.Tests.Geometory
     public sealed class Point2DTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PointZero_DefaultCoordinates_AreZero()
         {
-            Point2D pt = new Point2D(0, 0);
+            Assert.AreEqual(Point2D.Zero.X, 0);
 
-            Assert.AreEqual(pt, Point2D.Zero);
+            Assert.AreEqual(Point2D.Zero.Y, 0);
+
+            Assert.AreEqual(new Point2D(0, 0), Point2D.Zero);
+        }
+
+        [TestMethod]
+        public void Point2D_Transform_M()
+        {
+            Assert.AreEqual(Point2D.Zero.Transform(new Matrix2D(1, 0, 1, 0, 0, 0)), Point2D.Zero);
+            Assert.AreEqual(Point2D.Zero.Transform(new Matrix2D(0, 1, 0, 1, 0, 0)), Point2D.Zero);
+            Assert.AreEqual(Point2D.Zero.Transform(new Matrix2D(1, 1, 1, 1, 0, 0)), Point2D.Zero);
+
+            Assert.AreEqual(new Point2D(1, 1).Transform(new Matrix2D(1, 0, 1, 0, 0, 0)), new Point2D(1, 1));
+            Assert.AreEqual(new Point2D(1, 1).Transform(new Matrix2D(0, 1, 0, 1, 0, 0)), new Point2D(1, 1));
+            Assert.AreEqual(new Point2D(1, 1).Transform(new Matrix2D(1, 1, 0, 1, 0, 0)), new Point2D(2, 1));
+            Assert.AreEqual(new Point2D(1, 1).Transform(new Matrix2D(1, 1, 1, 1, 0, 0)), new Point2D(2, 2));
+
+            Assert.AreEqual(new Point2D(1, 1).Transform(new Matrix2D(-1, 0, -1, 0, 0, 0)), new Point2D(-1, -1));
+            Assert.AreEqual(new Point2D(1, 1).Transform(new Matrix2D(0, -1, 0, -1, 0, 0)), new Point2D(-1, -1));
+            Assert.AreEqual(new Point2D(1, 1).Transform(new Matrix2D(-1, -1, 0, -1, 0, 0)), new Point2D(-2, -1));
+            Assert.AreEqual(new Point2D(1, 1).Transform(new Matrix2D(-1, -1, -1, -1, 0, 0)), new Point2D(-2, -2));
+        }
+        public void Point2D_Transform_DXDY()
+        {
+
+            Assert.AreEqual(Point2D.Zero.Transform(new Matrix2D(0, 0, 0, 0, 1, 0)), new Point2D(1, 0));
+            Assert.AreEqual(Point2D.Zero.Transform(new Matrix2D(0, 0, 0, 0, 0, 1)), new Point2D(0, 1));
+            Assert.AreEqual(Point2D.Zero.Transform(new Matrix2D(0, 0, 0, 0, 1, 1)), new Point2D(1, 1));
+
+            Assert.AreEqual(Point2D.Zero.Transform(new Matrix2D(0, 0, 0, 0, -1, 0)), new Point2D(-1, 0));
+            Assert.AreEqual(Point2D.Zero.Transform(new Matrix2D(0, 0, 0, 0, 0, -1)), new Point2D(0, -1));
+            Assert.AreEqual(Point2D.Zero.Transform(new Matrix2D(0, 0, 0, 0, -1, -1)), new Point2D(-1, -1));
         }
     }
 }
