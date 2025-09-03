8fc70c8d6a9d1bf10424140b9de549d949124332
yuya-kambayashi <kambayashi73@gmail.com>
2025-09-03 11:15:48 +0900

    Testのサンプルをついかします

---
+++ PLN.Tests/Geometory/Point2DTests.cs
@@ -0,0 +1,16 @@
+﻿using PLN.Geometry;
+
+namespace PLN.Tests.Geometory
+{
+    [TestClass]
+    public sealed class Point2DTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Point2D pt = new Point2D(0, 0);
+
+            Assert.AreEqual(pt, Point2D.Zero);
+        }
+    }
+}
