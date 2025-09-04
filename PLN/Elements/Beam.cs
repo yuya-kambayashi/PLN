1a80120c64c92dfe80e2491f27e5d9d874d19fc1
yuya-kambayashi <kambayashi73@gmail.com>
2025-09-04 21:09:16 +0900

    Beamのプロパティの名称を変更します

--- PLN/Elements/Beam.cs
+++ PLN/Elements/Beam.cs
@@ -13,21 +13,21 @@ namespace PLN.Elements
 {
     internal class Beam : Line
     {
-        private float height;
-        private float width;
-        public float Height { get => height; set { height = value; } }
-        public float Width { get => width; set { width = value; } }
-        
-        public Beam(Line line, float height, float width)
-            : this(line.StartPoint, line.EndPoint, height, width)
+        private float h;
+        private float b;
+        public float H { get => h; set { h = value; } }
+        public float B { get => b; set { b = value; } }
+
+        public Beam(Line line, float h, float b)
+            : this(line.StartPoint, line.EndPoint, h, b)
         { }
-        public Beam(Point2D p1, Point2D p2, float height, float width)
+        public Beam(Point2D p1, Point2D p2, float h, float b)
         {
             StartPoint = p1;
             EndPoint = p2;
 
-            this.height = height;
-            this.width = width;
+            this.h = h;
+            this.b = b;
         }
         public override void Draw(Renderer renderer)
         {
@@ -38,34 +38,30 @@ namespace PLN.Elements
 
             Composite items = new Composite();
 
-            float Offset = 0.4f;
-
-            float tickSize = 0.5f * 100;
-
             // 始点側の底
-            Line tick1 = new Line(0, -tickSize + Offset, 0, tickSize + Offset);
-            tick1.Style = Style.ApplyLayer(Layer);
-            items.Add(tick1);
+            Line line1 = new Line(0, -b / 2, 0, b / 2);
+            line1.Style = Style.ApplyLayer(Layer);
+            items.Add(line1);
 
             // 終点側の底
-            Line tick2 = new Line(len, -tickSize + Offset, len, tickSize + Offset);
-            tick2.Style = Style.ApplyLayer(Layer);
-            items.Add(tick2);
+            Line line2 = new Line(len, -b / 2, len, b / 2);
+            line2.Style = Style.ApplyLayer(Layer);
+            items.Add(line2);
 
             // 始点から終点
-            Line tick3 = new Line(tick1.StartPoint, tick2.StartPoint);
-            tick3.Style = Style.ApplyLayer(Layer);
-            items.Add(tick3);
+            Line line3 = new Line(line1.StartPoint, line2.StartPoint);
+            line3.Style = Style.ApplyLayer(Layer);
+            items.Add(line3);
 
             // 終点から始点
-            Line tick4 = new Line(tick1.EndPoint, tick2.EndPoint);
-            tick4.Style = Style.ApplyLayer(Layer);
-            items.Add(tick4);
+            Line line4 = new Line(line1.EndPoint, line2.EndPoint);
+            line4.Style = Style.ApplyLayer(Layer);
+            items.Add(line4);
 
             Matrix2D trans = Matrix2D.Transformation(1, 1, angle, StartPoint.X, StartPoint.Y);
             items.TransformBy(trans);
 
-            foreach(var item in items)
+            foreach (var item in items)
             {
                 Line line = (Line)item;
 
